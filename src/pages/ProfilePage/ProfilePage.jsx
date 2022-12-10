9e88f77909082c45d06675720b7a18f2fa2082f3
kanthamas <kanthamas.iamrhod@gmail.com>
2022-12-10 17:26:37 +0700

    feat: add ProfilePage

---
+++ src/pages/ProfilePage/ProfilePage.jsx
@@ -0,0 +1,13 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable semi */
+import EditProfile from '../../components/EditProfile/EditProfile'
+
+function ProfilePage() {
+  return (
+    <div>
+      <EditProfile />
+    </div>
+  )
+}
+
+export default ProfilePage
