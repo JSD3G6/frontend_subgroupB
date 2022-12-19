a34c2dc8d8e3b3fd121eb1fc9d5d751d351cf57a
Pavit Pimchanagul <ppimchan40@gmail.com>
2022-12-19 16:19:36 +0700

    setup:AuthContext with Shared logic

--- src/contexts/authContext.js
+++ src/contexts/authContext.js
@@ -1,5 +1,8 @@
+/* eslint-disable react/jsx-no-constructed-context-values */
+/* eslint-disable no-unused-vars */
 /* eslint-disable react/prop-types */
-import { createContext, useContext } from 'react';
+// eslint-disable-next-line object-curly-newline
+import { createContext, useContext, useEffect, useState } from 'react';
 
 // ### Named Context
 const AuthContext = createContext();
@@ -7,8 +10,33 @@ const AuthContext = createContext();
 // ### Create HigherOrder Component : Context Wrapper
 
 function AuthContextProvider({ children }) {
-  // ## SHARED DATA & LOGIC(FUNCTION)
-  return <AuthContext.Provider>{children}</AuthContext.Provider>;
+  // ## SHARED DATA
+  const [user, setUser] = useState(null);
+  const [initialLoading, setInitialLoading] = useState(true);
+
+  // ## EFFECT HOOK
+  useEffect(() => {}, []);
+
+  // ## SHARED LOGIC
+  // # AUTH
+  const login = async (data) => {};
+  const logout = async (data) => {};
+  const register = async (data) => {};
+  // # PROFILE
+  const getUserProfile = async () => {};
+  const updateUserProfile = async (data) => {};
+
+  // # Bundle Shared Value,Logic in Object
+  const shared = {
+    user,
+    initialLoading,
+    login,
+    logout,
+    register,
+    getUserProfile,
+    updateUserProfile,
+  };
+  return <AuthContext.Provider value={shared}>{children}</AuthContext.Provider>;
 }
 
 // ### CustomHook
