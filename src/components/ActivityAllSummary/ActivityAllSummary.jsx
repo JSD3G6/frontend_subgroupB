eac951b7a480cce6b043540faf136d8004b3807b
kanthamas <kanthamas.iamrhod@gmail.com>
2022-12-19 18:11:17 +0700

    feat: add all types of activities bars

--- src/components/ActivityAllSummary/ActivityAllSummary.jsx
+++ src/components/ActivityAllSummary/ActivityAllSummary.jsx
@@ -1,24 +1,86 @@
 /* eslint-disable linebreak-style */
 /* eslint-disable react/jsx-one-expression-per-line */
 /* eslint-disable no-unused-vars */
-import { BiCycling } from 'react-icons/bi';
+import { BiCycling, BiSwim } from 'react-icons/bi';
+import { FaRunning, FaWalking } from 'react-icons/fa';
+import { GiHiking } from 'react-icons/gi';
 import ProgressBar from '../ProgressBar/ProgressBar';
 import './ActivityAllSummary.css';
 
 function ActivityAllSummary() {
-  const count = 8;
-  const totalDurationMin = (400 / 60).toFixed(2);
-  const percent = 50;
+  const count = 6;
+  const totalDurationMin = (700 / 60).toFixed(2);
+  const percent = 75;
   return (
     <div className="activity-all-container">
-      <div className="summary-container">
+      <div className="summary-container bicycling">
         <BiCycling className="activity-icon" />
         <span>{`Total: ${count} times`}</span>
-        <span className="total-duration-span">{ `${totalDurationMin} hours`}</span>
+        <span className="total-duration-span">{`${totalDurationMin} hours`}</span>
         <div className="acitivity-bar">
           <ProgressBar
             className="bicycling-bar"
-            percent={50}
+            percent={percent}
+            barColor="var(--purple)"
+            barWidth="18rem"
+            barHeight="0.75rem"
+          />
+          <span>{`${percent}%`}</span>
+        </div>
+      </div>
+      <div className="summary-container hiking">
+        <GiHiking className="activity-icon" />
+        <span>{`Total: ${count} times`}</span>
+        <span className="total-duration-span">{`${totalDurationMin} hours`}</span>
+        <div className="acitivity-bar">
+          <ProgressBar
+            className="hiking-bar"
+            percent={percent}
+            barColor="var(--purple)"
+            barWidth="18rem"
+            barHeight="0.75rem"
+          />
+          <span>{`${percent}%`}</span>
+        </div>
+      </div>
+      <div className="summary-container running">
+        <FaRunning className="activity-icon" />
+        <span>{`Total: ${count} times`}</span>
+        <span className="total-duration-span">{`${totalDurationMin} hours`}</span>
+        <div className="acitivity-bar">
+          <ProgressBar
+            className="running-bar"
+            percent={percent}
+            barColor="var(--purple)"
+            barWidth="18rem"
+            barHeight="0.75rem"
+          />
+          <span>{`${percent}%`}</span>
+        </div>
+      </div>
+      <div className="summary-container swimming">
+        <BiSwim className="activity-icon" />
+        <span>{`Total: ${count} times`}</span>
+        <span className="total-duration-span">{`${totalDurationMin} hours`}</span>
+        <div className="acitivity-bar">
+          <ProgressBar
+            className="swimming-bar"
+            percent={percent}
+            barColor="var(--purple)"
+            barWidth="18rem"
+            barHeight="0.75rem"
+          />
+          <span>{`${percent}%`}</span>
+        </div>
+      </div>
+      <div className="summary-container walking">
+        <FaWalking className="activity-icon" />
+        <span>{`Total: ${count} times`}</span>
+        <span className="total-duration-span">{`${totalDurationMin} hours`}</span>
+        <div className="acitivity-bar">
+          <ProgressBar
+            className="walking-bar"
+            percent={percent}
             barColor="var(--purple)"
             barWidth="18rem"
             barHeight="0.75rem"
