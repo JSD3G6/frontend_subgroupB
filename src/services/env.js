0ca7f4c655e286471508a78a489de79c6f8acec8
Pavit Pimchanagul <ppimchan40@gmail.com>
2022-12-20 22:31:05 +0700

    update endpoint

--- src/services/env.js
+++ src/services/env.js
@@ -1,3 +1,3 @@
 /* eslint-disable quotes */
 /* eslint-disable import/prefer-default-export */
-export const API_ENDPOINT_URL = "https://elitemove.vercel.app";
+export const API_ENDPOINT_URL = "http://18.219.121.147:8001";
