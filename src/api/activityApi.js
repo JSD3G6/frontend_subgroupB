d06e6e8392b69ebe7327920f924e29948bdc5ca4
Sitichai1995 <98759009+Sitichai1995@users.noreply.github.com>
2022-12-22 22:21:18 +0700

    add createActivity api

--- src/api/activityApi.js
+++ src/api/activityApi.js
@@ -1,6 +1,8 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable import/prefer-default-export */
 import axios from '../services/axios';
 
 export const getAllLazyLoad = (userId, page) => axios.get(`/activity/all/${userId}?pageSize=${page}`);
 export const deleteActivity = (activityId) => axios.delete(`/activity/${activityId}`);
 export const getActivity = (activityId) => axios.get(`/activity/${activityId}`);
+export const createActivity = (data) => axios.post('/activity', data);
